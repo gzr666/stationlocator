9c8e169b9dbb8537d58926e320865a01eda11cfb
geezer <bleksabat666@gmail.com>
2017-11-05 08:33:51 +0100

    openshift455

--- server.js
+++ server.js
@@ -91,9 +91,17 @@ app.use(function(req, res, next) {
 });
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+ var mongoUser = "geezer"; //mongodb username
+var mongoPass = "sabbath66"; //mongodb password
+var mongoDb   = "stationlocator"; //mongodb database name
+
+var mongoString = 'mongodb://' + mongoUser + ':' + mongoPass + '@' + 'ds243055.mlab.com:43055/stationlocator';
+
+   
+   
+   
 
 
 
@@ -107,25 +115,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 //mongo connectionstring
 //mongodb configuration open shift
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
 
 
 //OPEN SHIFT CHECK SERVER
@@ -138,7 +128,7 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 
 
 //mongo connection
-mongoose.connect(mongoURL);
+mongoose.connect(mongoString);
 var db = mongoose.connection;
 db.on("error",function(error){
 console.log(error);
